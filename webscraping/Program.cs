f05b916961713d314bb78096879d9b52c9db850f
Codeunit06 <fernando.brayan.m.g@gmail.com>
2022-11-04 02:05:07 -0600

    Web scraping con expresiones regulares

---
+++ webscraping/Program.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace webscraping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://codigofacilito.com/articulos/web-scraper");
+
+            foreach (var nodo in doc.DocumentNode.CssSelect(".article--content"))
+            {
+                var nodoancho = nodo.CssSelect("p").First();
+                lista.Add(nodoancho.InnerHtml);
+            }
+            
+            String search = @"[¿][A-Za-z áéíóú]*[?]";
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"Resultado:{item}");
+                MatchCollection matches = Regex.Matches(item, search);
+                foreach (Match m in matches)
+                {
+                    Console.WriteLine($"Encontre esto {m}");
+                }
+            }
+            
+        }
+    }
+}
